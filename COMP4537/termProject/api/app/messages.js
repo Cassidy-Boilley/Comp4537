9e7141556695b85f8f827cf81c28d05f352de305
Cassidy Boilley <cboilley@my.bcit.ca>
2024-04-06 17:55:57 -0700

    refactor for most paths

---
+++ COMP4537/termProject/api/app/messages.js
@@ -0,0 +1,8 @@
+exports.message = {
+    registrationSuccess: 'User registered successfully',
+    loginSuccess: 'Login successful',
+    invalidCredentials: 'Invalid username or password',
+    serverError: 'Internal Server Error',
+    usernameEmailExists: 'Username or email already exists',
+    apiFailed: 'Failed to fetch data from the external API'
+};
