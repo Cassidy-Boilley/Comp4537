b1673ca13e3e661842595bc876f77c125f29ebb1
Cassidy Boilley <cboilley@my.bcit.ca>
2024-04-08 00:23:49 -0700

    fix

--- COMP4537/termProject/scripts/userScreen.js
+++ COMP4537/termProject/scripts/userScreen.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', async () => {
             credentials: 'include'
         });
         const { apiCount } = await response.json();
-        document.getElementById('apiCountContainer').textContent = 'Remaining API calls: ' +  - Number(apiCount);
+        document.getElementById('apiCountContainer').textContent = 'Remaining API calls: ' +  MAXAPICALLS - Number(apiCount);
     } catch (error) {
         console.error('Error:', error);
         alert('An error occurred while fetching the API count. Please try again later.');
